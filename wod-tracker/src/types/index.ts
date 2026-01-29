8526bdad2be7b067148af7469b95464127a10f95
Kwakcena <rhkrgudwh@gmail.com>
2026-01-30 00:04:38 +0900

    feat: types 지정

---
+++ wod-tracker/src/types/index.ts
@@ -0,0 +1,21 @@
+export interface WodData {
+  [date: string]: string;
+}
+
+export interface Wod {
+  date: string;
+  content: string;
+}
+
+export interface WodCardProps {
+  wod: Wod;
+}
+
+export interface SearchBarProps {
+  searchTerm: string;
+  onSearchChange: (term: string) => void;
+}
+
+export interface WodListProps {
+  wods: Wod[];
+}
